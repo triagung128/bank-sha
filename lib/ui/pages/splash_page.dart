79402a1de5bc92da395c73fc82f000468963e236
triagung128 <triagung128@gmail.com>
2023-02-16 10:07:03 +0700

    create splash page

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img_logo_dark.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
