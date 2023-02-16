49dc441c706a30c26f1e1265c187c98258a0c903
triagung128 <triagung128@gmail.com>
2023-02-16 09:10:14 +0700

    added theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff14193F);
+Color greyColor = const Color(0xffA4A8AE);
+Color lightBackgroundColor = const Color(0xffF6F8FB);
+Color darkBackgroundColor = const Color(0xff020518);
+Color blueColor = const Color(0xff53C1F9);
+Color purpleColor = const Color(0xff5142E6);
+Color greenColor = const Color(0xff22B07D);
+Color numberBackgroundColor = const Color(0xff1A1D2E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle blueTextStyle = GoogleFonts.poppins(color: blueColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
